26454a4105fc8ddcf054ecced277f95d913a6796
ayushmina <66309981+ayushmina@users.noreply.github.com>
2021-04-01 18:13:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,3 +23,12 @@ io.on('connection', socket =>{
 
 
 })
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res) {
+  fs.readFile('index.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8000);
